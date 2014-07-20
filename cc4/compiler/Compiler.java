2d81a01b9bd96f9f738f9106b331cc03835bbf36
dfer <dfer@dfer.(none)>
2014-07-20 10:44:31 -0600

    Tarea #2, creando main en clase principal, analizando tags

--- cc4/compiler/Compiler.java
+++ cc4/compiler/Compiler.java
@@ -36,17 +36,6 @@ public class Compiler
 	public Compiler(){
 		super();
 	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public static void main() {
-		// TODO implement me	
-	}
 	
 	/**
 	 * <!-- begin-user-doc -->
@@ -59,6 +48,77 @@ public class Compiler
 		// TODO implement me
 		return false;	
 	}
-	
-}
 
+	/**
+	 * <!-- begin-user-doc -->
+	 Método para imprimir el menú de ayuda al usuario
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private static void help() {
+		// TODO implement me
+		System.out.println("*****	MENU DE AYUDA 	*****");
+		System.out.println("-o <outname>\n\tEscribir el output a un archivo de texto llamado <outname>.");
+		System.out.println("-target <stage>\n\tDonde <stage> es uno de: scan, parse, ast, semantic, irt, codegen; la compilación debe proceder hasta la etapa indicada.");
+		System.out.println("-opt <optimzation>\n\t<optimization> es uno de: constant, algebraic.");
+		System.out.println("-debug <stage>\n\tImprimir información de debugging.");
+		System.out.println("<stage> tiene las mismas opciones de -target, con la diferencia que se pueden 'debuggear' varias etapas, separandolas con ':' de la forma scan:parse:etc.");
+		System.out.println("-h\n\tMuestra esta ayuda al usuario.");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public static void main(String[] args) {
+		// TODO implement me
+		int h=0,o=0,target=0,opt=0,debug=0;
+		if (args.length>0) {
+			for (int i=0;i<args.length;i++) {
+				if (args[i].equals("-h")) {
+					if (h>0) {
+						h++;	
+					} else {
+						help();
+					}
+					h++;
+				} else if (args[i].equals("-o")) {
+					if (o>0) {
+						o++;	
+					} else {
+						System.out.println("ESTOY EJECUTANDO -o");	
+					}
+					o++;
+				} else if (args[i].equals("-target")) {
+					if (target>0) {
+						target++;
+					} else {
+						System.out.println("ESTOY EJECUTANDO -target");	
+					}
+					target++;
+				} else if (args[i].equals("-opt")) {
+					if (opt>0) {
+						opt++;	
+					} else {
+						System.out.println("ESTOY EJECUTANDO -opt");
+					}
+					opt++;
+				} else if (args[i].equals("-debug")) {
+					if (debug>0) {
+						debug++;	
+					} else {
+						System.out.println("ESTOY EJECUTANDO -debug");
+					}
+					debug++;	
+				}
+			}	
+		} else {
+			help();
+		}
+	}	
+}
